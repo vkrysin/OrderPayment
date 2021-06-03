619c87f58675f01250dc78747f495fbcb1d48c6c
Vladislav Krysin <krysin18@mail.ru>
2021-06-03 22:43:37 +0300

    basic project structure

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <payment-form></payment-form>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import PaymentForm from "./components/PaymentForm.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    PaymentForm,
   },
 };
 </script>
---
+++ src/components/PaymentForm.vue
@@ -0,0 +1,13 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "PaymentForm",
+}
+</script>
+
+<style>
+
+</style>
