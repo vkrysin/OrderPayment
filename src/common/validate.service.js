09282cf1dcf08ecf2f48cadf217356d06f773616
Vladislav Krysin <krysin18@mail.ru>
2021-06-11 21:34:02 +0300

    feat: email validation

--- src/common/validate.service.js
+++ src/common/validate.service.js
@@ -9,7 +9,7 @@ export const Validate = {
   /**
    * @param {string} phoneNumber '795984631887'
    * @returns {bool}
-  */
+   */
   isPhoneNumber(phoneNumber) {
     phoneNumber = phoneNumber.replace('+', '');
     return (
@@ -17,5 +17,11 @@ export const Validate = {
       phoneNumber.match(/[2-9]+/i)[0].length == 11 &&
       phoneNumber.length == 11
     );
-  }
-}
+  },
+
+  isEmail(email) {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  },
+};
