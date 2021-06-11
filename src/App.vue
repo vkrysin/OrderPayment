2261a44c4fcd9ddf59d9e0f2f86c705002748b56
Vladislav Krysin <krysin18@mail.ru>
2021-06-11 21:32:41 +0300

    new static content small fix in app.vue

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,23 @@
   <payment-form></payment-form>
 </template>
 <script>
-import PaymentForm from './components/PaymentForm.vue'
+import PaymentForm from './components/PaymentForm.vue';
 
 export default {
   name: 'App',
   components: {
-    PaymentForm
-  }
-}
+    PaymentForm,
+  },
+};
 </script>
 
 <style>
 @import url('scss/_fonts.scss');
-
 #app {
   /* font-family: Avenir, Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: rgba(230, 233, 240, 1);
+  min-height: 800px;
 }
 </style>
